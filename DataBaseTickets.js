6475584991c14fb749d22c50d12d83eca66525c3
Renat <suleymanov_renat@mail.ru>
2021-05-30 01:50:08 +0300

    connect with front

---
+++ DataBaseTickets.js
@@ -0,0 +1,16 @@
+tickets = [
+{
+    id: 01, // идентификатор (уникальный в пределах системы)
+    name: "Поломать принтер", // краткое описание
+    status: "done", // boolean - сделано или нет
+    created: null // дата создания (timestamp)
+},
+{
+    id: 02,
+    name: "Починить принтер",
+    status: "done",
+    created: null
+}
+];
+
+// module.exports = tickets;
--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-
 const Koa = require("koa");
 const logger = require("koa-logger");
 const koaBody = require("koa-body");
 const server = new Koa();
-
-import ticket from "./ticketsBD"
-// const ticket = {
-//     id: 01, // идентификатор (уникальный в пределах системы)
-//     name: "Petr", // краткое описание
-//     status: "done", // boolean - сделано или нет
-//     created: null // дата создания (timestamp)
-// }
+require("./DataBaseTickets")
 
 server.use(koaBody({
     urlencoded: true,
 }));
 
 server.use( async ctx => {
-    try {
-        ctx.response.body = ticket;
-
-    } catch (error) {
-        console.error("нет соединения: ", error);
-        ctx.body = "not connection";
-    }
-
     ctx.response.set({
         'Access-Control-Allow-Origin': '*',
     });
+    const method = ctx.request.querystring;
+ 
+    switch (method) { 
+        case "method=allTickets":
+            ctx.response.body = tickets;
+            return;
+        case "ticketById":
+            ctx.response.body = "zapros id";
+            return;
+        case "method=createTicket":
+            tickets.push({
+                name: "it's work!"
+            })
+            ctx.response.body = tickets;
+            return;
+        default:
+            ctx.response.status = 404;
+            return;
+    }   
 });
 
 server.listen(3000, () => {
