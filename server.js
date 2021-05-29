ee11053e7698c8b23e4835956f44650555d5f17e
Renat <suleymanov_renat@mail.ru>
2021-05-29 12:01:29 +0300

    ticketBD

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-// const http = require("http");
+
 const Koa = require("koa");
-// const logger = require("koa-morgan");
-// const koaBody = require("koa-body");
+const logger = require("koa-logger");
+const koaBody = require("koa-body");
 const server = new Koa();
 
-server.use(ctx => {
-    ctx.response.body = "na na na";
-    console.log("ctx.request.query");
+import ticket from "./ticketsBD"
+// const ticket = {
+//     id: 01, // идентификатор (уникальный в пределах системы)
+//     name: "Petr", // краткое описание
+//     status: "done", // boolean - сделано или нет
+//     created: null // дата создания (timestamp)
+// }
+
+server.use(koaBody({
+    urlencoded: true,
+}));
+
+server.use( async ctx => {
+    try {
+        ctx.response.body = ticket;
+
+    } catch (error) {
+        console.error("нет соединения: ", error);
+        ctx.body = "not connection";
+    }
+
     ctx.response.set({
         'Access-Control-Allow-Origin': '*',
-    })
-}).listen(3000)
+    });
+});
+
+server.listen(3000, () => {
+    console.log("соединение установлено")
+})
+
---
+++ ticketsBD.js
@@ -0,0 +1,8 @@
+const ticket = {
+    id: 01, // идентификатор (уникальный в пределах системы)
+    name: "Petr", // краткое описание
+    status: "done", // boolean - сделано или нет
+    created: null // дата создания (timestamp)
+}
+
+export default ticket;
